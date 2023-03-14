1e4a418052dad9f17b20c4ef430847643ff3b588
PytStudyOrg <forexampleorg@gmail.com>
2023-03-14 14:03:14 +0500

    changes

---
+++ task101.py
@@ -0,0 +1,10 @@
+print ( "Ведите: Фамилия Имя Отчество" )
+s = input()
+n = s.find ( " " )
+name = s [:n]
+s = s[n+1:]
+n = s.find ( " " )
+name2 = s [:n]
+s = s [n+1:]
+s = s + " " + name [0] + "." + name2 [0] + "."
+print (s)
