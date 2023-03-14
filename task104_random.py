49eb97cc9ea59a8e57e68ccd838c4332733e821c
PytStudyOrg <forexampleorg@gmail.com>
2023-03-14 15:48:56 +0500

    change

--- task104_random.py
+++ task104_random.py
@@ -1,4 +1,7 @@
 import random
 a = [(random.randint(0, 100) for x in range (100)]
+     avg = sum /len
+print = (a)
+
      
 
--- task105_sheffle.py
+++ task105_sheffle.py
@@ -1,3 +1,5 @@
+import random import shuffle
+
 N = int(input())
 
 A = [ randint(1,N) for x in range(N) ]
