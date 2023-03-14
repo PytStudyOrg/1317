9fc762a721fc6bcc1a543e691f91acb3d899ea3a
PytStudyOrg <forexampleorg@gmail.com>
2023-03-14 15:58:31 +0500

    changes

--- task105_sheffle.py
+++ task105_sheffle.py
@@ -1,15 +1,12 @@
-import random
-
+# import random
 from random import shuffle
 from random import randint
-
 import array
-
-N = int(input())
-
-A = [ randint(1,N) for x in range(N) ]
-
+N=int(input())
+A=[randint(1,N) for x in range (N)]
+print("Тип данных ", type(A))
 shuffle(A)
-
 print(A)
+
+
 
