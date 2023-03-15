4308afd486cf5cddc8e5f55bbd00a7cae1fe69e4
PytStudyOrg <forexampleorg@gmail.com>
2023-03-15 12:28:44 +0500

    changes

--- task110_max.py
+++ task110_max.py
@@ -1,15 +1,15 @@
 A = [1,8,3,4,5]
 N=len(A)
 M = A[0]
-# for i in range(1,N):
-#     if A[i] > M:
-#         M = A[i]
-# print(M)
+for i in range(1,N):
+     if A[i] > M:
+         M = A[i]
+ print(M)
 
-# for x in A:
-#     if x > M:
-#         M=x
-# print(M)
+ for x in A:
+     if x > M:
+         M=x
+ print(M)
 
 M=max(A)
 nMax=A.index(M)
