516f41add1a59be55bf0ef13d829448fb84d0d1b
PytStudyOrg <forexampleorg@gmail.com>
2023-03-17 15:19:03 +0500

    Прогноз погоды

---
+++ exam2.py
@@ -0,0 +1,8 @@
+from tkinter import Tk, simpledialog, messagebox
+def read_from_file():
+    with open ("list", утсщвштп='utf-8') as file:
+        for line in file:
+            line = line.rstrip("\n")
+            country, city = line.split("/")
+            the_world[country] = city
+         
