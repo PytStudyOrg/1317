a866df7b1df62bc2ec628e7a6e2e56e39e8ff3e5
PytStudyOrg <forexampleorg@gmail.com>
2023-03-17 16:51:29 +0500

    1

--- exam2.py
+++ exam2.py
@@ -1,10 +1,26 @@
-Проект№2 Столицы
+#Проект№2 Столицы
 
 from tkinter import Tk, simpledialog, messagebox
+
 def read_from_file():
-    with open ("list", утсщвштп='utf-8') as file:
+    with open ("capital_data.txt", encoding='utf-8') as file:
         for line in file:
             line = line.rstrip("\n")
             country, city = line.split("/")
             the_world[country] = city
+
+print ("Словарь столиц")
+root = Tk()
+root.withdraw()
+the_world{}
          
+der read_from_file():
+
+while True:
+    query_country = simpledialog.askstring("Страна", "Введите название страны")
+    if query_country in the_world:
+        rusult = the_world[query_country]
+        messagebox.showinfo("Ответ", query_country + " : столица этой страны - " + result + " ! ")
+    else:
+        print ("Нет такой страны!")
+
